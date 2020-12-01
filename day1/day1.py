6d0ab5930f4739a800359def3804b997f9d245b4
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2020-12-01 12:31:23 +0000

    Solution to day1 part2

--- day1/day1.py
+++ day1/day1.py
@@ -1,17 +1,23 @@
 from itertools import combinations
+from functools import reduce
 
-def pair_add(numlist: list[int]) -> dict:
+def ntuple_add(numlist: list[int], tuple_size: int) -> dict:
     """
     This probably isn't the best way to do this
     """
-    for pair in combinations(numlist, 2):
-        yield {"sum": sum(pair), "pair": pair}
+    for n_tuple in combinations(numlist, tuple_size):
+        yield {"sum": sum(n_tuple), "tuple": n_tuple}
+
+def get_results(numlist: list[int], tuple_size: int):
+    for results in ntuple_add(numlist, tuple_size):
+        if results["sum"] == 2020:
+            print(results["tuple"])
+            print(reduce(lambda x, y: x * y, results["tuple"]))
+            break
+
 
 with open("input.txt") as f:
     numbers = [int(line.strip()) for line in f]
 
-for results in pair_add(numbers):
-    if results["sum"] == 2020:
-        a, b = results["pair"]
-        print(a, b)
-        print(a * b)
+get_results(numbers, 2)
+get_results(numbers, 3)
