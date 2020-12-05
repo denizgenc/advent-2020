350a87674b0e0c9b3a15246f8d6ddc8b71524289
Deniz Genc <25902330+denizgenc@users.noreply.github.com>
2020-12-05 04:44:29 +0000

    Refactor day2 solution to prepare for part2

--- day2.py
+++ day2.py
@@ -1,21 +1,27 @@
-from typing import List
+from typing import Callable, List
 
 import adventinput
 
 
-def password_validator(password_list: List[str]) -> int:
+def password_validator(
+    password_list: List[str], policy: Callable[[str, str, int, int], bool]
+) -> int:
     num_valid = 0
     for line in password_list:
         counts, letter, password = line.split(" ")
-        min_count, max_count = [int(s) for s in counts.split("-")]
+        num1, num2 = [int(s) for s in counts.split("-")]
         letter = letter[0]
-        if password.count(letter) in range(min_count, max_count + 1):
+        if policy(password, letter, num1, num2):
             num_valid += 1
 
     return num_valid
 
 
+def old_policy(password: str, letter: str, min_count: int, max_count: int) -> bool:
+    return password.count(letter) in range(min_count, max_count + 1)
+
+
 passwords = adventinput.get_data(2)
 
-day2_part1_answer = password_validator(passwords)
+day2_part1_answer = password_validator(passwords, old_policy)
 print(f"Number of valid passwords = {day2_part1_answer}")
